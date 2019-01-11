4a334d02b10016268f82b83f30429aca89187f83
Adam Goldschmidt <adamgold7@gmail.com>
2019-01-11 10:57:43 +0200

    finish tests

--- src/pypkgfreezer.py
+++ src/pypkgfreezer.py
@@ -1,13 +1,28 @@
 import pkg_resources
+from typing import Iterable
 try:
     from pip._internal.operations import freeze
 except ImportError:  # pip < 10.0
     from pip.operations import freeze
 
 
-def get_pkgs():
+def get_pkgs() -> Iterable[list]:
     """run pip freeze and make a list of [name, version]"""
     return (line.strip().split("==") for line in freeze.freeze())
+
+
+def parse_setup_file() -> dict:
+    """parse setup.py file to make requirements lists"""
+
+
+def freeze_pkgs(old_lists: dict, new_lists: dict, add_new: bool = False):
+    """ take old lists of requirements and insert versions
+    add_new configures whether to add new pkgs inside new_lists
+    that don't exist in old_lists"""
+
+
+def alter_setup_file(new_lists: dict):
+    """ take new_lists and insert them to setup.py"""
 
 
 def main():
--- tests/test_pypkgfreezer.py
+++ tests/test_pypkgfreezer.py
@@ -1,4 +1,5 @@
-from src.pypkgfreezer import get_pkgs
+from src.pypkgfreezer import (get_pkgs, parse_setup_file,
+                              freeze_pkgs, alter_setup_file)
 
 
 def test_pip_output():
@@ -10,8 +11,26 @@ def test_pip_output():
 def test_setup_parse():
     """take setup.py and get lists-install_requires,
     test_requires, setup_requires"""
+    parse_setup = parse_setup_file()
+    assert isinstance(parse_setup['install'], list)
+    assert isinstance(parse_setup['test'], list)
+    assert isinstance(parse_setup['setup'], list)
+
+
+def test_insert_versions():
+    """test that parsed lists and pip output
+    make for new lists with versions"""
+    parse_setup = parse_setup_file()
+    pkgs = freeze_pkgs(parse_setup, get_pkgs(), add_new=False)
+    assert len(pkgs['install']) == len(parse_setup['install'])
+    assert len(pkgs['test']) == len(parse_setup['test'])
+    assert len(pkgs['setup']) == len(parse_setup['setup'])
 
 
 def test_setup_altered():
     """ alter setup.py with altered lists"""
-    pass
+    parse_setup = parse_setup_file()
+    pkgs = freeze_pkgs(parse_setup, get_pkgs(), add_new=False)
+    alter_setup_file(pkgs)
+    new_parse_setup = parse_setup_file()
+    assert new_parse_setup == pkgs
