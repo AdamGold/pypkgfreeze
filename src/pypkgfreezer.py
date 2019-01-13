a45c190238848296077cd80691208b365db7fe60
Adam Goldschmidt <adamgold7@gmail.com>
2019-01-13 14:37:49 +0200

    implement freeze_pkgs logic

--- src/pypkgfreezer.py
+++ src/pypkgfreezer.py
@@ -1,4 +1,5 @@
 import pkg_resources
+import itertools
 from typing import Iterable
 try:
     from pip._internal.operations import freeze
@@ -11,18 +12,25 @@ def get_pkgs() -> Iterable[list]:
     return (line.strip().split("==") for line in freeze.freeze())
 
 
-def parse_setup_file() -> Iterable[str]:
+def parse_setup_file() -> [[list]]:
     """parse setup.py file to make requirements lists"""
 
 
-def freeze_pkgs(old_lists: Iterable[str], new_lists: Iterable[list],
-                add_new: bool = False) -> Iterable:
+def freeze_pkgs(old_lists: Iterable[str], new_list: Iterable[list],
+                add_new: bool = False) -> Iterable[(str, list)]:
     """take old lists of requirements and insert versions
     add_new configures whether to add new pkgs inside new_lists
     that don't exist in old_lists"""
+    repeating_new_list = itertools.cycle(new_list)
+    for (name, requirements) in old_lists.items():
+        for (pkg, version) in repeating_new_list:
+            for (i, req) in enumerate(requirements):
+                if pkg in req:
+                    requirements[i] == f"{pkg}=={version}"
+        yield (name, requirements)
 
 
-def alter_setup_file(new_lists: Iterable[str]):
+def alter_setup_file(new_lists: Iterable[(str, list)]):
     """ take new_lists and insert them to setup.py"""
 
 
